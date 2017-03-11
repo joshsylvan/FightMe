04b8b7097e19e9bfbeaa8da12812add8a9f72315
Joshua Gabrel <joshgabrel@live.co.uk>
2017-03-11 20:05:25 +0000

    Warrior parry

---
+++ Assets/ParryHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParryHandler : MonoBehaviour {
+
+	public Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		if (GetComponent<Animator> () == null) {
+			transform.parent.gameObject.GetComponent<Animator> ();
+		} else {
+			anim = GetComponent<Animator> ();
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision col){
+		if (col.gameObject.tag == "PlayerWeapon" || col.gameObject.tag == "PlayerShield") {
+			//if (anim.GetCurrentAnimatorStateInfo (0).IsName ("anim_WarriorSlash")) {
+			Debug.Log("Parry");
+				anim.SetTrigger ("Parry");
+			//}
+		}
+	}
+}
